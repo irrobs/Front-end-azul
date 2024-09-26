d3f37e01b70f3aae5ac52627798135ab34beaf25
Thiago Maicky <thiagomaicky@gmail.com>
2024-09-26 19:17:40 -0300

    Códi da About

--- src/front-azul/about/About.jsx
+++ src/front-azul/about/About.jsx
@@ -1,3 +1,18 @@
+import"./About.css"
 export default function About() {
-  return <div>About</div>;
+  <div class="conteiner">
+  <h2> ℹ️ SOBRE NÓS</h2>
+  
+  <div class="section">
+    <strong>Back-End:</strong>
+    <p id="backend_infor"></p>
+  </div>
+  <div class="section">
+    <strong>Front-End:</strong>
+    <p id="frontend_infor"></p>
+    
+  </div>
+  
+  
+</div>
 }
