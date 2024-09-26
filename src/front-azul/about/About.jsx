e38c001ee361a84e6b45b6033cb493e7c3fb660e
Matheus Borri <matheus.escobar90@gmail.com>
2024-09-26 20:20:01 -0300

    corrigido bug about e sidebar

--- src/front-azul/about/About.jsx
+++ src/front-azul/about/About.jsx
@@ -1,18 +1,18 @@
-import"./About.css"
+import "./About.css";
+
 export default function About() {
-  <div class="conteiner">
-  <h2> ℹ️ SOBRE NÓS</h2>
-  
-  <div class="section">
-    <strong>Back-End:</strong>
-    <p id="backend_infor"></p>
-  </div>
-  <div class="section">
-    <strong>Front-End:</strong>
-    <p id="frontend_infor"></p>
-    
-  </div>
-  
-  
-</div>
+  return (
+    <div className="conteiner">
+      <h2> ℹ️ SOBRE NÓS</h2>
+
+      <div className="section">
+        <strong>Back-End:</strong>
+        <p id="backend_infor"></p>
+      </div>
+      <div className="section">
+        <strong>Front-End:</strong>
+        <p id="frontend_infor"></p>
+      </div>
+    </div>
+  );
 }
