82aeacb7cfa990a12cdce6858fbd2c4b0a4c8af9
CirsioDani <danielroleite@gmail.com>
2024-10-18 12:01:14 -0300

    Adicionei em info o título e parte do mapa, montei as divs e parte do css

--- src/front-azul/info/Info.jsx
+++ src/front-azul/info/Info.jsx
@@ -1,3 +1,16 @@
-export default function Info() {
-  return <div>Info</div>;
+import "./info.css";
+
+export default function info() {
+  return (
+    <div className="container">
+      <div class="container-titulo">
+        <div class="titulo"> Lixo Eletrônico </div>
+      </div>
+      
+      <div class="container-mapa">
+        <div class="mapa"> Mapa </div>
+      </div>
+
+    </div>
+  );
 }
