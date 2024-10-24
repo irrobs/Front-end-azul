6fda03364f92da097a232ef07249ba3c52974df8
unknown <danielroleite@gmail.com>
2024-10-24 18:39:03 -0300

    Mudanças na orientação da pagina info e em estilos, falta colocar algumas imagens ainda

--- src/front-azul/info/Info.jsx
+++ src/front-azul/info/Info.jsx
@@ -3,39 +3,43 @@ import "./info.css";
 export default function info() {
   return (
     <div className="container">
-      <div class="container-titulo">
-        <div class="titulo"> Lixo Eletrônico </div>
+      
+        <div class="titulo"> LOCALIZAÇÕES PARA DESCARTE </div>
+
+      <div class="sub-container">
+
+        <div class="container-mapa">
+          <div class="mapa"> Mapa </div>
+        </div>
+
+        <div class="tabela">
+          <table class="custom-table">
+
+            <thead>
+              <tr>
+                <th title="Aqui nesta coluca está o nome da localização em questão!"> Nome </th>
+                <th title="Aqui nesta coluna está a dintância entre você e o local de descarte!"> Distância </th>
+                <th title="Aqui nesta coluna está a avaliação do local de descarte!"> Avaliação </th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td> Ferro Velho </td>
+                <td><i class="carro"></i> 5 km </td>
+                <td> 4 </td>
+              </tr>
+              <tr>
+                <td> Ferro Velho </td>
+                <td><i class="carro"></i> 5 km </td>
+                <td> 4 </td>
+              </tr>
+            </tbody>
+
+          </table>
+        </div>
+
       </div>
-
-      <div class="container-mapa">
-        <div class="mapa"> Mapa </div>
-      </div>
-
-      <div class="tabela">
-        <table class="custom-table">
-          <thead>
-            <tr>
-              <th>Nome</th>
-              <th>Distância</th>
-              <th>Avaliação</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td> Ferro Velho </td>
-              <td><i class="carro"></i> 5 km </td>
-              <td> 4 </td>
-            </tr>
-            <tr>
-              <td> Ferro Velho </td>
-              <td><i class="carro"></i> 5 km </td>
-              <td> 4 </td>
-            </tr>
-          </tbody>
-        </table>
-
-      </div>
-
     </div>
   );
 }
