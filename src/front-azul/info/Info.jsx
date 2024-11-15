731b31c83b068831e64f6ad4d5d3b3a8790c1948
Matheus Borri <matheus.escobar90@gmail.com>
2024-11-15 03:19:41 -0300

    implementação do mapa e geração de marcadores para o local

--- src/front-azul/info/Info.jsx
+++ src/front-azul/info/Info.jsx
@@ -1,24 +1,41 @@
 import { useLocation } from "react-router-dom";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "./info.css";
+import "leaflet/dist/leaflet.css";
 
 export default function Info() {
   const location = useLocation();
-  console.log(location.state.locais);
+  const locais = location.state.locais;
 
   return (
     <div className="container">
       <div className="titulo"> LOCALIZAÇÕES PARA DESCARTE </div>
 
       <div className="sub-container">
-        <div className="container-mapa">
-          <div className="mapa"> Mapa </div>
-        </div>
+        <MapContainer
+          center={[locais[0].latitude, locais[0].longitude]}
+          zoom={16}
+          scrollWheelZoom={true}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {locais.map((local) => (
+            <Marker
+              position={[local.latitude, local.longitude]}
+              key={local.latitude}
+            >
+              <Popup>{local.nome}</Popup>
+            </Marker>
+          ))}
+        </MapContainer>
 
         <div className="tabela">
           <table className="custom-table">
             <thead>
               <tr>
-                <th title="Aqui nesta coluca está o nome da localização em questão!">
+                <th title="Aqui nesta coluna está o nome da localização em questão!">
                   {" "}
                   Nome{" "}
                 </th>
@@ -34,20 +51,15 @@ export default function Info() {
             </thead>
 
             <tbody>
-              <tr>
-                <td> Ferro Velho </td>
-                <td>
-                  <i className="carro"></i> 5 km{" "}
-                </td>
-                <td> 4 </td>
-              </tr>
-              <tr>
-                <td> Ferro Velho </td>
-                <td>
-                  <i className="carro"></i> 5 km{" "}
-                </td>
-                <td> 4 </td>
-              </tr>
+              {locais.map((local) => (
+                <tr key={local.latitude}>
+                  <td> {local.nome} </td>
+                  <td>
+                    <i className="carro"></i> 5 km{" "}
+                  </td>
+                  <td> {local.avaliacao} </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
